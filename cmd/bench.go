4f083e7b1fe7501160d222dbd17999fa8a7ccdd6
amirhnajafiz <najafizadeh21@gmail.com>
2024-05-15 21:16:39 +0330

    init: bench cobra

--- cmd/bench.go
+++ cmd/bench.go
@@ -1,1 +1,20 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+type Bench struct{}
+
+func (b Bench) Command() *cobra.Command {
+	return &cobra.Command{
+		Short: "bench",
+		Long:  "benchmark",
+		Use:   "benchmark EMQX cluster",
+		Run: func(_ *cobra.Command, _ []string) {
+			b.main()
+		},
+	}
+}
+
+func (b Bench) main() {
+
+}
