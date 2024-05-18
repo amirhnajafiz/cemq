a84ffec0fcb23b5969b3463935f2e2a9b709a5d1
amirhnajafiz <najafizadeh21@gmail.com>
2024-05-18 13:09:34 +0330

    add: config info method

--- internal/commands/config/config.go
+++ internal/commands/config/config.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 
 	"github.com/amirhnajafiz/cemq/internal/utils"
+	"github.com/amirhnajafiz/cemq/pkg/model"
 )
 
 type config struct{}
 
+// Info reads the current config file
+func (c config) Info() string {
+	path := fmt.Sprintf("%s/%s/%s", ROOT, BASE, CONTEXT)
+
+	config, err := utils.ReadFile(path)
+	if err != nil {
+		return "failed to read current config!"
+	}
+
+	obj, err := utils.ReadJSON[*model.Config](config)
+	if err != nil {
+		return fmt.Errorf("failed to read %s config file: %w", config, err).Error()
+	}
+
+	return fmt.Sprintf("%v", obj)
+}
+
 // Select method is used to change the current context
-func Select(name string) string {
+func (c config) Select(name string) string {
 	path := fmt.Sprintf("%s/%s/%s", ROOT, BASE, CONTEXT)
 	out := fmt.Sprintf("%s/%s/%s/%s.json", ROOT, BASE, CONFIGS, name)
 
