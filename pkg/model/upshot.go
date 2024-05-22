9e3636fa1ebe1219490a582de3f7892a4fb84bc0
amirhnajafiz <najafizadeh21@gmail.com>
2024-05-22 21:12:25 +0330

    update: upshot type

--- pkg/model/upshot.go
+++ pkg/model/upshot.go
@@ -1,21 +1,33 @@
 package model
 
+import "fmt"
+
+type StatType string
+
+const (
+	PubStat StatType = "publish"
+	SubStat StatType = "subscribe"
+)
+
 // Upshot stores a benchmark result
 type Upshot struct {
 	ID    string
 	Topic string
-	Pubs  []*PubStat
-	Subs  []*SubStat
+	Stats []*Stat
 }
 
-type PubStat struct {
-	ID              string
-	WriteThroughput float64
-	WriteMps        float64
+func (u Upshot) ToString() string {
+	return fmt.Sprintf("Benchmark %s: topic %s", u.ID, u.Topic)
 }
 
-type SubStat struct {
-	ID             string
-	ReadThroughput float64
-	ReadMps        float64
+// Stat is a single entity that holds benchmark data
+type Stat struct {
+	ID         string
+	Type       string
+	Throughput float64
+	Mps        float64
+}
+
+func (s Stat) ToString() string {
+	return fmt.Sprintf("[%s] %s: %f mps, %f kbyte/s", s.ID, s.Type, s.Mps, s.Throughput)
 }
