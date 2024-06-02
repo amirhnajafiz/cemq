bc650c5b64d3a134d34985b778360b3293e67d69
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-02 16:15:22 +0330

    fix: installer profile bug

--- installer/install.sh
+++ installer/install.sh
@@ -2,12 +2,12 @@
 
 # Function to detect profile file
 detect_profile() {
-    if [[ -f "$HOME/.bash_profile" ]]; then
+    if [[ -f "$HOME/.zshrc" ]]; then
+        echo "$HOME/.zshrc"
+    elif [[ -f "$HOME/.bash_profile" ]]; then
         echo "$HOME/.bash_profile"
     elif [[ -f "$HOME/.bashrc" ]]; then
         echo "$HOME/.bashrc"
-    elif [[ -f "$HOME/.zshrc" ]]; then
-        echo "$HOME/.zshrc"
     else
         echo "No profile file found."
         exit 1
@@ -18,13 +18,10 @@ detect_profile() {
 profile_file=$(detect_profile)
 
 # Path to be added, assuming pwa is a variable holding path
-pwa="$( pwd )/cemq"  # Change this path to your executable path
+pwa="$( pwd )"  # Change this path to your executable path
 
 # Adding PATH to user profile
-echo "export PATH=\"\$PATH:$pwa\"" >> "$profile_file"
+echo "export PATH=\"$pwa:\$PATH\"" >> "$profile_file"
 echo "PATH updated in $profile_file"
-
-# Reloading profile
-source "$profile_file"
 
 echo "CEMQ installation and setup complete."
